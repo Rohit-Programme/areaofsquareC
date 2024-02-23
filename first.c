cacb4bb9d2f689a961c2f93c591b190f1623b77b
Rohit <rohitkusv@gmail.com>
2024-02-23 11:56:38 +0530

    Add files via upload
    #include<stdio.h>
    int main(){
        int side;
        printf("enter the side of circle");
        scanf("%d",&side);
        int area =side*side;
        printf("the area of a circle is a =%d",area);
    }

---
+++ first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int side;
+    printf("enter the side of circle");
+    scanf("%d",&side);
+    int area =side*side;
+    printf("the area of a circle is a =%d",area);
+}
